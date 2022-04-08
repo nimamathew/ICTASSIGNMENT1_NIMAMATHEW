23049b3c4445b909628bc6ca0b1c79bdb2a7579c
Nima Anu Mathew <nimamathew@gmail.com>
2022-04-08 14:24:05 +0530

    psdv2

--- js/main.js
+++ js/main.js
@@ -95,6 +95,69 @@
     return true;
    }
 
+   function CheckPasswordStrength(password) {
+    var password_strength = document.getElementById("password_strength");
+
+    //TextBox left blank.
+    if (password.length == 0) {
+        password_strength.innerHTML = "";
+        return;
+    }
+
+    //Regular Expressions.
+    var regex = new Array();
+    regex.push("[A-Z]"); //Uppercase Alphabet.
+    regex.push("[a-z]"); //Lowercase Alphabet.
+    regex.push("[0-9]"); //Digit.
+    // regex.push("[$@$!%*#?&]"); //Special Character.
+
+    var passed = 0;
+
+    //Validate for each Regular Expression.
+    for (var i = 0; i < regex.length; i++) {
+        if (new RegExp(regex[i]).test(password)) {
+            passed++;
+        }
+    }
+
+    //Validate for length of Password.
+    if (passed > 2  && password.length > 8) {
+        passed++;
+    }
+
+    if (password.length > 8) 
+    {
+       alert("password not allowed to more than 8 charaters");
+       return false;
+    }
+
+    //Display status.
+    var color = "";
+    var strength = "";
+     switch (passed) {
+        case 0:
+        case 1:
+            strength = "Weak";
+            color = "red";
+            break;
+        case 2:
+            strength = "Good";
+            color = "darkorange";
+            break;
+        case 3:
+        case 4:
+            strength = "Strong";
+            color = "green";
+            break;
+        case 5:
+            strength = "Very Strong";
+            color = "darkgreen";
+            break;
+    }
+    password_strength.innerHTML = strength;
+    password_strength.style.color = color;
+}
+
 function validatePassword()
 {
 
